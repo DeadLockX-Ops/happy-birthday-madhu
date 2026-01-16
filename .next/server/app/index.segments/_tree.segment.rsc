bd742f08c8d6bc9a2e06627ad6b1573b4a018e4d
The-Freelauncer <piyushdutta0007@gmail.com>
2026-01-16 11:10:17 +0530

    Minor changes

--- .next/server/app/index.segments/_tree.segment.rsc
+++ .next/server/app/index.segments/_tree.segment.rsc
@@ -3,4 +3,4 @@
 :HL["/_next/static/media/84780b0176be2d72-s.p.3e21df15.woff2","font",{"crossOrigin":"","type":"font/woff2"}]
 :HL["/_next/static/media/970c428219233a3d-s.p.feb96725.woff2","font",{"crossOrigin":"","type":"font/woff2"}]
 :HL["/_next/static/chunks/92e69286dd7eb550.css","style"]
-0:{"buildId":"C0CHu_23NIl3NUA0Hotlj","tree":{"name":"","paramType":null,"paramKey":"","hasRuntimePrefetch":false,"slots":{"children":{"name":"__PAGE__","paramType":null,"paramKey":"__PAGE__","hasRuntimePrefetch":false,"slots":null,"isRootLayout":false}},"isRootLayout":true},"staleTime":300}
+0:{"buildId":"NckqIzMaF9MuEnqnsqvsb","tree":{"name":"","paramType":null,"paramKey":"","hasRuntimePrefetch":false,"slots":{"children":{"name":"__PAGE__","paramType":null,"paramKey":"__PAGE__","hasRuntimePrefetch":false,"slots":null,"isRootLayout":false}},"isRootLayout":true},"staleTime":300}
--- src/components/screens/MessageScreen.jsx
+++ src/components/screens/MessageScreen.jsx
@@ -27,7 +27,7 @@ export default function MessageScreen({ onNext }) {
                     className=" h-auto max-w-xl bg-gradient-to-br from-pink-200 via-pink-100 to-pink-50 rounded-2xl shadow-lg p-4 md:p-6 text-center"
                 >
                     <p className="text-[#301733] text-base md:text-lg leading-relaxed overflow-y-auto max-h-[400px] pr-2">
-                        Happy Birthday, Madhu <3 !
+                        Happy Birthday, Madhu ❤️ !
                         Some people don’t just enter your life — they leave a mark on it, and you’ve done that so effortlessly just by being who you are. You have this rare way of making people feel seen, appreciated, and at ease, and it’s something truly special.
 
                         Before you came into my life, things felt dull and heavy in ways I never spoke about. Somehow, your presence turned all of that into something lighter, warmer — almost like finding heaven after a long, difficult time. It’s something I’ve come to cherish deeply. There’s a softness in your kindness that stays with me long after our conversations end, and knowing you has become something I hold close to my heart.
@@ -35,7 +35,7 @@ export default function MessageScreen({ onNext }) {
                         You inspire feelings that are rare and beautiful, and I admire you more than words can explain. I hope today reminds you of how much you matter and how much light you bring into my life.
 
                         Wishing you a year filled with happiness, love, and moments that make you feel as special as you truly are.
-                        – 143 💗
+                         143 💗
                     </p>
                 </motion.div>
             </div>
